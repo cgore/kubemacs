5038daacfcc30d9ecb13548b206b62a50fc81c42
Christopher Mark Gore <chris.gore@rate.com>
2019-11-26 21:54:14 -0600

    Starting to add a config selector

--- kubemacs.el
+++ kubemacs.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+(require 'ht)
+
 (defconst kubemacs-buffer-name "*kubemacs*"
   "This is the buffer name for the standard Kubemacs buffer, defaulting to *kubemacs*.")
 
@@ -11,24 +13,55 @@
   "Return: The kubemacs buffer, creating it if necessary first."
   (get-buffer-create kubemacs-buffer-name))
 
+(defvar kubemacs-selected-config-key
+  nil
+  "This is the currently selected kubemacs configuration.")
+
+(defun kubemacs-select-config ()
+  (interactive)
+  (setq kubemacs-selected-config-key
+        (x-popup-menu t (list (format "Select a new Kubemacs config (currently %s):"
+                                      kubemacs-selected-config-key)
+                              (cons "Kubemacs Config Keys"
+                                    (mapcar (lambda (config-key)
+                                              (cons (format "%s" config-key) config-key))
+                                            (ht-keys kubemacs-config)))))))
+
+(maphash (lambda (config-key config-map)
+           (cons (format "%s" :foo) :foo))
+         kubemacs-config)
+
+(defvar kubemacs-mode-map
+  (let* ((map (make-sparse-keymap))
+         (k (lambda (keys def)
+              (define-key map (kbd keys) def))))
+    (funcall k "C" 'kubemacs-select-config)
+    map))
+
+(define-derived-mode kubemacs-mode special-mode "Kubemacs"
+  "Major Mode for use in the Kubemacs buffer."
+  (use-local-map kubemacs-mode-map))
+
 (defun kubemacs ()
   "Kubernetes Emacs UI."
   (interactive)
   (with-current-buffer (kubemacs-buffer)
     (erase-buffer)
     (maphash (lambda (env-key env-config)
+               (insert (propertize (format "%s" env-key) 'face 'bold) "\n")
                (insert (propertize "--------------------------------------------------------------\n"
                                    'face 'bold))
-               (insert (propertize (format "%s" env-key) 'face 'bold) "\n")
                (mapcar (lambda (config-key)
                          (insert "\t" (propertize (format "%s" config-key) 'face 'bold)
                                  " " (gethash config-key env-config)
                                  "\n"))
                        '(:kubectl-path :context))
+               (insert (propertize "get namespaces\n" 'face 'italic))
                (call-process (gethash :kubectl-path env-config)
                              nil t nil
                              "--context" (gethash :context env-config)
                              "get" "namespaces"))
-             kubemacs-config)))
+             kubemacs-config)
+    (kubemacs-mode)))
 
 ;;; kubemacs.el ends here
