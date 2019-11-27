76447d33d4c6b073e2432b6ed13943c4958afe71
Christopher Mark Gore <chris.gore@rate.com>
2019-11-26 20:31:35 -0600

    basic kubectl output

--- kubemacs.el
+++ kubemacs.el
@@ -4,9 +4,6 @@
 
 ;;; Code:
 
-(defvar kubemacs-kubectl-path "kubectl"
-  "Path of the kubectl executable to use.")
-
 (defconst kubemacs-buffer-name "*kubemacs*"
   "This is the buffer name for the standard Kubemacs buffer, defaulting to *kubemacs*.")
 
@@ -19,7 +16,19 @@
   (interactive)
   (with-current-buffer (kubemacs-buffer)
     (erase-buffer)
-    (call-process kubemacs-kubectl-path nil t nil "version")
-    ))
+    (maphash (lambda (env-key env-config)
+               (insert (propertize "--------------------------------------------------------------\n"
+                                   'face 'bold))
+               (insert (propertize (format "%s" env-key) 'face 'bold) "\n")
+               (mapcar (lambda (config-key)
+                         (insert "\t" (propertize (format "%s" config-key) 'face 'bold)
+                                 " " (gethash config-key env-config)
+                                 "\n"))
+                       '(:kubectl-path :context))
+               (call-process (gethash :kubectl-path env-config)
+                             nil t nil
+                             "--context" (gethash :context env-config)
+                             "get" "namespaces"))
+             kubemacs-config)))
 
 ;;; kubemacs.el ends here
